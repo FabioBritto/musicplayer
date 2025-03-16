1f87a3c8f0ec9268b72937ec22ccf9a2baf26293
FabioBritto <fabio.tritono@gmail.com>
2025-03-16 20:23:57 -0300

    Alterada estrutura do enum Estilo

--- src/main/java/br/com/fabiobritto/musicplayer/model/enums/Estilo.java
+++ src/main/java/br/com/fabiobritto/musicplayer/model/enums/Estilo.java
@@ -2,10 +2,29 @@ package br.com.fabiobritto.musicplayer.model.enums;
 
 public enum Estilo {
 
-	POP,
-	GOSPEL,
-	ROCK,
-	CLASSICO,
-	SERTANEJO,
-	COUNTRY;
+	POP(1),
+	GOSPEL(2),
+	ROCK(3),
+	CLASSICO(4),
+	SERTANEJO(5),
+	COUNTRY(6);
+
+	private int value;
+	
+	Estilo(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public static Estilo valueOf(int value) {
+		for(Estilo e : Estilo.values()) {
+			if(e.getValue() == value) {
+				return e;
+			}
+		}
+		throw new IllegalArgumentException("Valor inválido");
+	}
 }
